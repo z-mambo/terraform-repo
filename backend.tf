23222431860a325de2f523220ae58d6905926614
zita <myzitam@yahoo.com>
2021-07-22 20:30:01 -0600

    backend

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state-s3-backend"
     key = "jt.tfstate"
-    region = "us-west-2"
+    region = "us-east-2"
     dynamodb_table = "terraform"
   }
 }
